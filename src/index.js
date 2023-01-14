08c45de3548d0a14d98ad61e38ba44ad3c86602d
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-01-14 14:29:45 +0500

    adding font-awesome icons

--- src/index.js
+++ src/index.js
@@ -7,50 +7,64 @@ import user from './img/user1.jpg'
 import comment from './img/comment.png'
 import share from './img/share.png'
 import like from './img/thumbsUp.png'
+//import 'https://kit.fontawesome.com/92529d23a5.js'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
-function Post() {
+function Post(props) {
   return (
     <div className="post">
+
       <div className="post__header">
         <img className="post__avatar" src={user} alt="avatar" />
         <div className="post__user-info">
-          <span className="post__author">John Doe</span>
+          <span className="post__author">{props.name}</span>
           <br></br>
-          <span className="post__date">Today at 5:00PM</span>
+          <span className="post__date">{props.date}</span>
         </div>
       </div>
+
       <div className="post__content">
-        <p className="post__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec ultricies tincidunt, nunc elit ultricies nunc, eget ultricies lorem nisl eget nunc. Sed euismod, nisl nec ultricies tincidunt, nunc elit ultricies nunc, eget ultricies lorem nisl eget nunc.</p>
+        <p className="post__text">{props.postText}</p>
         <img className="post__image" src={post} alt="post" />
       </div>
+
       <div className="post__footer">
         <div className="post__stats">
           <div className='container'>
             <div className='row'>
               <div className='col-4'>
                 <div className="post__stat">
-                  <img className="post__icon" src={like} />
-                  <span className="post__stat-number">2.3K</span>
+                  <i className="fa-solid fa-thumbs-up icon fa-xl"></i>
+                  <span className="post__stat-number">{props.likes}</span>
                 </div>
               </div>
               <div className='col-4'>
                 <div className="post__stat">
-                  <img className="post__icon" src={comment} />
-                  <span className="post__stat-number">2.3K</span>
+                  <i className="fa-solid fa-comment icon fa-xl"></i>
+                  <span className="post__stat-number">{props.comments}</span>
                 </div>
               </div>
               <div className='col-4'>
                 <div className="post__stat">
-                  <img className="post__icon" src={share} />
-                  <span className="post__stat-number">2.3K</span>
+                  <i className="fa-solid fa-share icon fa-xl"></i>
+                  <span className="post__stat-number">{props.shares}</span>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+
     </div>
   );
 }
 
-ReactDOM.render(<Post />, document.querySelector("#root"));
+ReactDOM.render(<Post
+  name='Alice'
+  date='10th Jan, 2023'
+  postText='10th Jan, 2023'
+  likes='2k'
+  comments='25'
+  shares='10'
+/>, document.querySelector("#root"));
